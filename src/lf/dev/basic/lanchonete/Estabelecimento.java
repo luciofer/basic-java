4a68bffbc03aced038c3cf4b3eb73f7c14684e3e
Lucio Araujo <contatolucioaraujo@gmail.com>
2023-10-17 00:18:13 -0300

    added lanchonete package

---
+++ src/lf/dev/basic/lanchonete/Estabelecimento.java
@@ -0,0 +1,26 @@
+package lf.dev.basic.lanchonete;
+
+import lf.dev.basic.lanchonete.area.cliente.Cliente;
+import lf.dev.basic.lanchonete.atendimento.Atendente;
+import lf.dev.basic.lanchonete.atendimento.cozinha.Cozinheiro;
+
+public class Estabelecimento {
+    public static void main(String[] args) {
+        Cozinheiro cozinheiro = new Cozinheiro();
+
+        //ações que estabelecimento precisa ter ciência
+        cozinheiro.adicionarSucoNoBalcao();;
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarComboNoBalcao();
+
+        Atendente atendente = new Atendente();
+        atendente.receberPagamento();
+        atendente.servindoMesa();
+
+        Cliente cliente = new Cliente();
+        cliente.escolherLanche();
+        cliente.fazerPedido();
+        cliente.pagarConta();
+
+    }
+}
